26c76acbc3806af18857823a67b6a583be833b13
Anna <annasharamko2606@gmail.com>
2024-06-18 18:23:18 +0300

    work

---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+function checkForSpam(message) {
+  var lowerCaseMessage = message.toLowerCase();
+  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
